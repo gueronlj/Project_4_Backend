c485580f93b55cd5f24a8d4ad4513a787f6effaf
SoNesky <gueronlj@gmail.com>
2021-11-18 03:34:52 -0500

    every user has collection array

--- lazy_dev_api/models.py
+++ lazy_dev_api/models.py
@@ -1,10 +1,13 @@
+from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
 # Create your models here.
+
 class User(models.Model):
     name = models.CharField(max_length=32)
     password = models.CharField(max_length=128)
     online = models.BooleanField()
+    collection = ArrayField(models.IntegerField(), null=True, blank=True)
 
 class Guide(models.Model):
     name = models.CharField(max_length=32)
